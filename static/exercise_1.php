f2207a324683a49056afdab4a91b008d77b1bc2b
Alaska T. González <Alaska45l@users.noreply.github.com>
2023-04-06 15:06:05 -0700

    completed exercise_4

--- static/exercise_1.php
+++ static/exercise_1.php
@@ -1,45 +1,17 @@
-<?php
-
-// array asociativo
-
-$cats_status = [
-  "0" => "Chimuelo",
-  "1" => "Akira",
-  "2" => "Chispita",
-  "3" => "Wendy",
-  "4" => "Batato",
-  "5" => "Michu"
-];
-
-// array indexados
-
-$number = [1, 2, 3, 4, 5, 6];
-
-?>
-
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../style/style.css">
-  <title>Cat status</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Document</title>
 </head>
-
 <body>
-  <div class="container">
-    <h1>CAT STATUS</h1>
-    <ul class="cat_ul">
-      <?php
-      for ($i = 0; $i < count(array_keys($cats_status)); $i++) {
-        $list = '<li> ' . $number[$i] . ' - ' . $cats_status[$i] . '</li>';
-        echo $list;
-      }
-      ?>
-    </ul>
-  </div>
+    <div class="container">
+        <h1>
+            Hola Mundo
+        </h1>
+    </div>
 </body>
-
 </html>
--- static/exercise_4.php
+++ static/exercise_4.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 // array asociativo
 
 $cats_status = [
@@ -8,12 +10,21 @@ $cats_status = [
   "2" => "Chispita",
   "3" => "Wendy",
   "4" => "Batato",
-  "5" => "Michu"
+  "5" => "Michu",
+  "6" => "Burbuja",
+  "7" => "Tornillo",
+  "8" => "Wilson"
 ];
 
 // array indexados
 
-$number = [1, 2, 3, 4, 5, 6];
+$number = [2017, 2017, 2017, 2015, 2012, 2006, 2004, 2003, 1994];
+
+// tamano de lista
+
+$tamano = $_GET["tamano"];
+
+$cantidadLista = $tamano;
 
 ?>
 
@@ -31,9 +42,17 @@ $number = [1, 2, 3, 4, 5, 6];
 <body>
   <div class="container">
     <h1>CAT STATUS</h1>
+    <div class="buttons">
+      <?php
+      echo "<a href='?tamano=2'>2 Gatos</a>";
+      echo "<a href='?tamano=4'>4 Gatos</a>";
+      echo "<a href='?tamano=6'>6 Gatos</a>";
+      echo "<a href='?tamano=8'>8 Gatos</a>";
+      ?>
+    </div>
     <ul class="cat_ul">
       <?php
-      for ($i = 0; $i < count(array_keys($cats_status)); $i++) {
+      for ($i = 0; $i < $cantidadLista; $i++) {
         $list = '<li> ' . $number[$i] . ' - ' . $cats_status[$i] . '</li>';
         echo $list;
       }
